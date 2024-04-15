8ab92f73c2725c51a7114c0de56a3613d513d747
Nikole Morales <88848356+nikole-mor@users.noreply.github.com>
2024-04-15 08:42:28 -0600

    Update Ejercicios_10.js

--- Desarrollo Web/javascript_basics/Ejercicios_10.js
+++ Desarrollo Web/javascript_basics/Ejercicios_10.js
@@ -1,1 +1,15 @@
-Ejercicios_10.js
+//fucnion que recibe la cadena de entrada para saber si es palindromo o no
+    function palindromo(cadena) {
+          //se modifica la entrada para que pueda ser evaluada correctamete
+        const cadenaModificada = cadena.toLowerCase().replace(/[^a-z0-9]/g, '');
+          // se comparan las cadenas para saber si el palindromo o no
+        const esPalindromo = cadenaModificada === cadenaModificada.split('').reverse().join('');
+        //regresa el output con el formato pedido
+        return esPalindromo ? `La cadena ${cadena} es palíndromo`: `La cadena ${cadena} NO es palíndromo`;
+    }
+    
+    //se imprimen los casos de uso
+    console.log(palindromo('anita lava la tina'));
+    console.log(palindromo('revolver')); 
+    console.log(palindromo('1001')); 
+    
