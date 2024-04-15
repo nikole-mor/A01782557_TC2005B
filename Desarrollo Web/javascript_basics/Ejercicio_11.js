37ec9d0702b41594e82d41093ba18a96f59f9bec
Nikole Morales <88848356+nikole-mor@users.noreply.github.com>
2024-04-15 08:50:16 -0600

    Update Ejercicio_11.js

--- Desarrollo Web/javascript_basics/Ejercicio_11.js
+++ Desarrollo Web/javascript_basics/Ejercicio_11.js
@@ -1,1 +1,19 @@
-Ejercicio_11.js
+function ordenarCadenas(listaCadenas) {
+    // toma la lista  de cadenas de texto y devuelve una nueva lista con las cadenas ordenadas.
+    return listaCadenas.slice().sort();
+}
+
+//casos de prueba
+const casosPrueba = [
+    ["manzana", "banana", "uva", "pera", "kiwi"],
+    ["pajaro", "tigre", "gato", "aguila"],
+    ["azul", "rojo", "verde", "amarillo"]
+];
+
+//se imprimen los casos de prubeas
+casosPrueba.forEach((listaCadenas) => {
+    const cadenasOrdenadas = ordenarCadenas(listaCadenas);
+    console.log("original:", listaCadenas);
+    console.log("ordenada:", cadenasOrdenadas);
+    console.log("----------------------");
+});
