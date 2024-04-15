f4c6852b91b93a6cfc4d099fb8b9716b4a0fd654
Nikole Morales <88848356+nikole-mor@users.noreply.github.com>
2024-04-15 09:26:52 -0600

    Update Ejercicio_15.js

--- Desarrollo Web/javascript_basics/Ejercicio_15.js
+++ Desarrollo Web/javascript_basics/Ejercicio_15.js
@@ -1,1 +1,21 @@
-Ejercicio_15.js
+//funcion que recibe una lista de nuero para oredenarlos de manera descendente
+function ordenDescendente(listaNumeros) {
+    //listaNumeros.slice() crea una copia de la lista
+    //sort() para ordenar de forma descendente los numeros de la lista
+    return listaNumeros.slice().sort((a, b) => b - a);
+}
+
+// array de casos de prueba
+const casosPrueba = [
+    [3, 1, 4, 1, 5, 9, 2, 6, 5],
+    [10, 5, 8, 3, 9, 2],
+    [7, 3, 1, 6, 4, 2, 8]
+];
+
+// se ejecutan los casos de prueba
+casosPrueba.forEach((num) => {
+    const numerosOrdenados = ordenDescendente(num);
+    console.log("lista:", num);
+    console.log("ordenados:", numerosOrdenados);
+    console.log("----------------------");
+});
