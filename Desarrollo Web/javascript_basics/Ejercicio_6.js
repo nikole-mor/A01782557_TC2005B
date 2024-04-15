ffd1dccc88c41a4249bc1ace5ed1f3a5b059f77f
Nikole Morales <88848356+nikole-mor@users.noreply.github.com>
2024-04-15 07:41:13 -0600

    Update Ejercicio_6.js

--- Desarrollo Web/javascript_basics/Ejercicio_6.js
+++ Desarrollo Web/javascript_basics/Ejercicio_6.js
@@ -1,1 +1,34 @@
-Ejercicio_6.js
+function hackerSpeak(cadena) {
+    // array con el equivalente de numeros a cada letra
+    const letra_num = [
+        ['a', '4'],
+        ['e', '3'],
+        ['i', '1'],
+        ['o', '0'],
+        ['s', '5']
+    ];
+
+    // se conveirte la cadena a hacker speaj
+    let hackerText = cadena; //si inicia la variable con la cadena de entrada
+    letra_num.forEach(letra_num => {
+        //se crea una reges para buscar similitudes
+        const regex = new RegExp(letra_num[0], 'gi'); //gi significa es busqueda global
+        hackerText = hackerText.replace(regex, letra_num[1]); //remplanza las ocurrencias 
+    });
+
+    return hackerText;
+}
+
+// lista con casos de prueba
+const casosPrueba = [
+    'Javascript es divertido','Hola mundo','Programando en javascript'
+];
+
+// se ejecutan las pruebas
+casosPrueba.forEach((cadena, index) => {
+    const resultado = hackerSpeak(cadena);
+    console.log(`prueba ${index + 1}:`);
+    console.log("input:", cadena);
+    console.log("hacker speak:", resultado);
+    console.log("----------------------");
+});
